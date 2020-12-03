445e84a915dbe7e9883cf49127d4f9a27b496244
Võ Minh Duy <minhduy.ringo@gmail.com>
2020-12-03 11:16:48 +0700

    Clean game source

--- game/src/scenes/index.ts
+++
@@ -1,6 +0,0 @@
-import { BootState }             from './boot';
-import { PreloadScene }     from './preload';
-import { MainMenuScene }    from './main-menu-scene';
-import { GameScene }        from './game-scene';
-
-export default [BootState, PreloadScene, MainMenuScene, GameScene];
